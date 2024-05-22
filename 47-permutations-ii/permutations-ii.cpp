90b0a00d82a4c48f0a80cd36fa002e947bf119c0
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-05-22 09:59:57 +0530

    Time: 3 ms (89.10%) | Memory: 14 MB (24.27%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    void Permutation(vector<int>nums,int ind,vector<int>temp,vector<vector<int>>&ans,vector<int>visited)
+    void Permutation(vector<int>nums,int ind,vector<vector<int>>&ans)
     {
-        if(ind == nums.size())
+        if(ind == (nums.size()-1))
         {
-            ans.push_back(temp);
-            return;
+            ans.push_back(nums);
         }
-        map<int,int>mp;
-        for(int i = 0 ; i < nums.size() ; i++ )
+        for(int i = ind ; i < nums.size() ; i++ )
         {
-            // Here I Need To Make sure that Only Unique's Should be Passed
-            if(visited[i] == 0 && mp[nums[i]]==0)
+            if(ind!=i)
             {
-               visited[i] = 1;
-               temp.push_back(nums[i]);
-               mp[nums[i]]++;
-               Permutation(nums,ind+1,temp,ans,visited);
-               visited[i] = 0;
-               temp.pop_back();
+               if(nums[i]!=nums[ind])
+               {
+                swap(nums[i],nums[ind]);
+                Permutation(nums,ind+1,ans);
+               }
+            }
+            else
+            {
+                Permutation(nums,ind+1,ans);
             }
         }
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        vector<int>temp;
-        vector<int>visited(nums.size()+1,0);
-
-        Permutation(nums,0,temp,ans,visited);
-        // vector<vector<int>>flex;
-        // for(auto it : ans)
-        // {
-        //     flex.push_back(it);
-        // }
+        Permutation(nums,0,ans);
         return ans;
     }
 };
 
 // Now You need to Optimize This Code Right Now
+
+// (The Much More Optimized Solution for this question is like somewhere)
