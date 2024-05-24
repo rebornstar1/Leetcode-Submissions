4b791dffaf44564c95905e7343126443ee7ff89c
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-05-25 00:49:15 +0530

    Time: 511 ms (5.09%) | Memory: 224.3 MB (5.09%) - LeetSync

---
+++ 1788-stone-game-vi/stone-game-vi.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        int n = aliceValues.size();
+        vector<vector<int>>vt;
+        for(int i = 0 ; i < n ; i++ )
+        {
+            vector<int>temp;
+            temp.push_back(aliceValues[i]+bobValues[i]);
+            temp.push_back(aliceValues[i]);
+            temp.push_back(bobValues[i]);
+            vt.push_back(temp);
+        }
+        sort(vt.begin(),vt.end());
+        int alice = 0, bob = 0;
+        int flag = 0;
+        for(int i = (n-1) ; i >= 0 ; i-- )
+        {
+            if(flag == 0)
+            {
+                alice+=vt[i][1];
+                flag = 1;
+            }
+            else
+            {
+                bob+=vt[i][2];
+                flag = 0;
+            }
+        }
+
+        if(alice > bob) return 1;
+        if(bob > alice) return -1;
+        return 0;
+    }
+};
