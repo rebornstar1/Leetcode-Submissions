64649d49477d9cefa30083e639eacf9a912dc363
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-05-25 01:04:46 +0530

    Time: 226 ms (27.73%) | Memory: 122.6 MB (25.63%) - LeetSync

--- 1788-stone-game-vi/stone-game-vi.cpp
+++ 1788-stone-game-vi/stone-game-vi.cpp
@@ -3,27 +3,26 @@ public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
         int n = aliceValues.size();
         // Instead of Using Vector of Vector You can Declear the Custom Sorting Method
-        vector<vector<int>>vt;
-        for(int i = 0 ; i < n ; i++ )
+        priority_queue<pair<int,pair<int,int>>>pq;
+        for(int i = 0 ; i < aliceValues.size() ; i++)
         {
-            vector<int>temp;
-            temp.push_back(aliceValues[i]+bobValues[i]);
-            temp.push_back(i);
-            vt.push_back(temp);
+            pq.push({aliceValues[i]+bobValues[i],{aliceValues[i],bobValues[i]}});
         }
-        sort(vt.begin(),vt.end());
-        int alice = 0, bob = 0;
-        int flag = 0;
-        for(int i = (n-1) ; i >= 0 ; i-- )
+
+        int alice = 0, bob = 0, flag  = 0;
+        while(!pq.empty())
         {
+            int as = pq.top().second.first;
+            int bs = pq.top().second.second;
+            pq.pop();
             if(flag == 0)
             {
-                alice+=aliceValues[vt[i][1]];
+                alice+=as;
                 flag = 1;
             }
             else
             {
-                bob+=bobValues[vt[i][1]];
+                bob+=bs;
                 flag = 0;
             }
         }
