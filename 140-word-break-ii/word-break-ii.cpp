1bf4cd7f309d7faaee69079893763c40c568ba6d
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-05-25 10:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (49.07%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void check(int ind,string s,set<string>&qst, vector<string>&champ, vector<vector<string>>&ans)
+    void check(int ind,string s,set<string>&qst, string champ, vector<string>&ans)
     {
         if(ind == s.size())
         {
@@ -14,39 +14,28 @@ public:
             cp+=s[i];
             if(qst.find(cp) != qst.end())
             {
-                champ.push_back(cp);
+                string houp = champ;
+                champ+=cp;
+                if(i != s.size()-1) champ+=' ';
                 check(i+1,s,qst,champ,ans);
-                champ.pop_back();
+                champ = houp;
             }
         }
     }
  
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         // Finding the Maximum Length Word in wordDict
-        int n = 0;
         set<string>qst;
         for(int i = 0 ; i < wordDict.size() ; i++ )
         {
             int p = wordDict.size();
-            n = max(n,p);
             qst.insert(wordDict[i]);
         }
         string temp;
-        vector<vector<string>>ans;
-        vector<string>champ;
+        vector<string>ans;
+        string champ;
         check(0,s,qst,champ,ans);
         
-        vector<string>jump;
-        for(int i = 0 ; i < ans.size() ; i++ )
-        {
-            string keep;
-            for(int j = 0 ; j < ans[i].size() ; j++)
-            {
-              keep+=ans[i][j];
-              if(j != ans[i].size()-1) keep+=' ';
-            }
-            jump.push_back(keep);
-        }
-        return jump;
+        return ans;
     }
 };
