18d677e1c2f7acb9e2dbade3ddcb3803e0b0c4f2
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-05-25 12:08:47 +0530

    Time: 250 ms (8.86%) | Memory: 53.1 MB (11.71%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,30 +5,33 @@ public:
         if(grid[0][0] == 1) return -1;
         int n = grid.size();
         int m = grid[0].size();
-        vector<pair<int,int>> adj[n+1][m+1];
-        for(int i = 0 ; i < n ; i++ )
-        {
-            for(int j = 0 ; j < n ; j ++ )
-            {
-                if(grid[i][j] == 0)
-                {
-                    // Now It's Adjacent which are also 0
-                    for(int k = -1 ; k <= 1 ; k++ )
-                    {
-                        for(int p = -1 ; p <= 1 ; p++ )
-                        {
-                            if(k == 0 && p == 0) continue;
-                            int u = i+k;
-                            int v = j+p;
-                            if(u >= 0 && u < n && v >= 0 && v < m && grid[u][v] == 0)
-                            {
-                              adj[i][j].push_back({u,v});
-                            }
-                        }
-                    }
-                }
-            }
-        }
+
+        // Let's Just Try This Question Without Using Adjacency List
+
+        // vector<pair<int,int>> adj[n+1][m+1];
+        // for(int i = 0 ; i < n ; i++ )
+        // {
+        //     for(int j = 0 ; j < n ; j ++ )
+        //     {
+        //         if(grid[i][j] == 0)
+        //         {
+        //             // Now It's Adjacent which are also 0
+        //             for(int k = -1 ; k <= 1 ; k++ )
+        //             {
+        //                 for(int p = -1 ; p <= 1 ; p++ )
+        //                 {
+        //                     if(k == 0 && p == 0) continue;
+        //                     int u = i+k;
+        //                     int v = j+p;
+        //                     if(u >= 0 && u < n && v >= 0 && v < m && grid[u][v] == 0)
+        //                     {
+        //                       adj[i][j].push_back({u,v});
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         // Adjacency List Is Created
 
         // I Will Apply Breadth First Search Here
@@ -50,23 +53,41 @@ public:
             int j = pq.top()[2];
             pq.pop();
 
-            for(int k = 0 ; k < adj[i][j].size() ; k++ )
+            for(int k = -1 ; k <= 1 ; k++ )
             {
-                int u = adj[i][j][k].first;
-                int v = adj[i][j][k].second;
-                if((wet+1) < dist[u][v])
+                for(int f = -1 ; f <= 1 ; f++ )
                 {
-                    dist[u][v] = (wet+1);
-                    vector<int>temp;
-                    temp.push_back(dist[u][v]);
-                    temp.push_back(u);
-                    temp.push_back(v);
-                    pq.push(temp);
+                    if(k == 0 && f == 0 ) continue;
+                    int u = i+k;
+                    int v = j+f;
+                    if(u >= 0 && u < n && v >= 0 && v < m && grid[u][v] == 0)
+                    {
+                        if((wet+1) < dist[u][v])
+                        {
+                            dist[u][v] = (wet+1);
+                            vector<int>gpa;
+                            gpa.push_back(dist[u][v]);
+                            gpa.push_back(u);
+                            gpa.push_back(v);
+                            pq.push(gpa);
+                        }
+                    }
                 }
             }
+        }
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            for(int j = 0 ; j < n ; j++ )
+            {
+                cout<<dist[i][j]<<" ";
+            }
+            cout<<endl;
         }
 
         if(dist[n-1][m-1] == INT_MAX) return -1;
         return dist[n-1][m-1];
     }
 };
+
+// Optimize This Code Now
