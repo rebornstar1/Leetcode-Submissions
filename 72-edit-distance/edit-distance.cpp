8ad6e2f1ec6fa26519fc5c727ff59c3a5256bc67
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-06-08 12:59:49 +0530

    Time: 14 ms (24.54%) | Memory: 12 MB (53.66%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,37 +1,80 @@
 class Solution {
 public:
-    int cntDistance(string &word1, string &word2, int i, int j, vector<vector<int>>&dp)
-    {
-        // Base Case is yet to be decided
-        if(j == word2.size())
-        {
-            return (word1.size()-i); 
-        }
+    // int cntDistance(string &word1, string &word2, int i, int j, vector<vector<int>>&dp)
+    // {
+    //     // Base Case is yet to be decided
+    //     if(j == word2.size())
+    //     {
+    //         return (word1.size()-i); 
+    //     }
 
-        if(i == word1.size())
-        {
-            return (word2.size()-j);
-        }
+    //     if(i == word1.size())
+    //     {
+    //         return (word2.size()-j);
+    //     }
 
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
+    //     if(dp[i][j] != -1)
+    //     {
+    //         return dp[i][j];
+    //     }
 
-        if(word1[i] == word2[j])
-        {
-            return dp[i][j] = cntDistance(word1,word2,i+1,j+1,dp); // Next Traversal
-        }
-        else
-        {
-            return dp[i][j] = (1+min({cntDistance(word1,word2,i+1,j+1,dp),cntDistance(word1,word2,i+1,j,dp),cntDistance(word1,word2,i,j+1,dp)}));
-        }
-    }
+    //     if(word1[i] == word2[j])
+    //     {
+    //         return dp[i][j] = cntDistance(word1,word2,i+1,j+1,dp); // Next Traversal
+    //     }
+    //     else
+    //     {
+    //         return dp[i][j] = (1+min({cntDistance(word1,word2,i+1,j+1,dp),cntDistance(word1,word2,i+1,j,dp),cntDistance(word1,word2,i,j+1,dp)}));
+    //     }
+    // }
 
     int minDistance(string word1, string word2) {
+        // Think About the Tabulation Approach in this question
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return cntDistance(word1,word2,0,0,dp);
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        // Base Case
+        for(int i = 0 ; i < n ; i++ ) dp[i][word2.size()] = (word1.size()-i);
+        for(int j = 0 ; j < m ; j++ ) dp[word1.size()][j] = (word2.size()-j);
+
+        // for(int i = 0 ; i <= n ; i++ )
+        // {
+        //     for(int j = 0 ; j <= m ; j++ )
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        // cout<<endl;
+        // cout<<endl;
+
+        for(int i = (n-1) ; i >= 0 ; i-- )
+        {
+            for(int j = (m-1) ; j >= 0 ; j-- )
+            {
+                if(word1[i] == word2[j]) // Same Character
+                {
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j] = (1 + min({dp[i+1][j+1],dp[i+1][j],dp[i][j+1]}));
+                }
+            }
+        }
+
+        // for(int i = 0 ; i <= n ; i++ )
+        // {
+        //     for(int j = 0 ; j <= m ; j++ )
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        return dp[0][0];
     }
 };
