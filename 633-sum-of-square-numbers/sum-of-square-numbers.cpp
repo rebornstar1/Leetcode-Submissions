3c63c93ab37c7ea7254559640ea0278fc9f2b7cb
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-06-17 12:37:51 +0530

    Time: 553 ms (5.00%) | Memory: 260.1 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     bool judgeSquareSum(long long c) {
         vector<long long>squares;
+        unordered_map<long long,long long>mp;
         set<long long>check;
         squares.push_back(0);
+        mp[0]++;
         for(long long i = 1 ; i*i <= (c) ; i++ )
         {
             squares.push_back(i*i);
-            check.insert(i*i);
-        }
-
-        for(auto it : squares)
-        {
-            cout<<it<<" ";
+            mp[i*i]++;
         }
         
         // Checking the c-a*a does exit or not in the set
+        unordered_map<long long,long long>temp;
         for(long long i = 0 ; i < squares.size() ; i++ )
         {
-            if(check.find(c-squares[i]) != check.end() || c == squares[i])
-            {
-                return true;
-            }
+            // Required Value is  c-squares[i] respectively
+            temp[c-squares[i]]++;
+        }
+
+        for(auto it : temp )
+        {
+           if(mp[it.first] != 0) return true;
         }
         return false;
     }
 };
+
+// Currently The Time Complexity is quite high try to resolve that fast
