b08142830bb14b474b99e7a812c0a6876a40fb27
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-06-18 08:25:28 +0530

    Time: 604 ms (14.82%) | Memory: 147.9 MB (5.03%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    static bool cmp(int a,int b)
+    static bool cmpvec(vector<int> a,vector<int> b)
     {
         return a > b; // This is for Decreasing order
     }
 
+    static bool cmp(int a,int b)
+    {
+        return a > b;
+    }
+
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        priority_queue<vector<int>>pq;
+        vector<vector<int>>pq;
         int n = difficulty.size();
         for(int i = 0 ; i < n ; i++ )
         {
-            pq.push({profit[i],difficulty[i]});
+            pq.push_back({profit[i],difficulty[i]});
         }
+        sort(pq.begin(),pq.end(),cmpvec);
         sort(worker.begin(),worker.end(),cmp);
         int ans = 0;
+        int ind = 0;
         for(int i = 0 ; i < worker.size() ; i++ )
         {
-            while(pq.top()[1] > worker[i])
-            {
-               pq.pop();
-               if(pq.empty()) return ans;
-            }
-            ans += pq.top()[0];
+               while(pq[ind][1] > worker[i])
+               {
+                 ind++;
+                 if(ind == n) return ans;
+               }
+
+             ans += pq[ind][0];
         }
         return ans;
     }
