8b3c5f1fe4e5a493294f88ac6c592e1ea6375c64
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-06-18 08:18:25 +0530

    Time: 110 ms (21.91%) | Memory: 49.4 MB (9.41%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool cmp(int a,int b)
+    {
+        return a > b; // This is for Decreasing order
+    }
+
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        priority_queue<vector<int>>pq;
+        int n = difficulty.size();
+        for(int i = 0 ; i < n ; i++ )
+        {
+            pq.push({profit[i],difficulty[i]});
+        }
+        sort(worker.begin(),worker.end(),cmp);
+        int ans = 0;
+        for(int i = 0 ; i < worker.size() ; i++ )
+        {
+            while(pq.top()[1] > worker[i])
+            {
+               pq.pop();
+               if(pq.empty()) return ans;
+            }
+            ans += pq.top()[0];
+        }
+        return ans;
+    }
+};
