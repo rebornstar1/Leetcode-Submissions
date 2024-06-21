f18834c435b70853cfe70b0f4913ded601a1de36
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-06-21 10:40:49 +0530

    Time: 24 ms (59.49%) | Memory: 34.4 MB (47.97%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -3,19 +3,17 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int sum = 0;
         int n = customers.size();
-        for(int i = 0 ; i < n ; i++ )
-        {
-            if(grumpy[i] == 0)
-            {
-                sum += customers[i];
-            }
-        }
 
         // Now We will Apply 2 pointer Approach
         int temp = 0, ans = 0;
         int curr = 0, prev = 0;
         while(curr != n)
         {
+            if(grumpy[curr] == 0)
+            {
+                sum += customers[curr];
+            }
+
             if((curr-prev) < minutes)
             {
                 if(grumpy[curr] == 1){
@@ -41,3 +39,5 @@ public:
         return ans+sum;
     }
 };
+
+// Think About Can you optimise It More ?
