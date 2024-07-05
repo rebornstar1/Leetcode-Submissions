8ec6a5765361c99e1284f7ca82db35bc5104d71d
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-07-05 18:18:07 +0530

    Time: 101 ms (89.38%) | Memory: 11.9 MB (57.93%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int cntCoins(vector<int>& nums,int i,int j,vector<vector<int>>&dp)
-    {
-        // BaseCase Need to be written here
-        if(i > j) return 0;
+    // int cntCoins(vector<int>& nums,int i,int j,vector<vector<int>>&dp)
+    // {
+    //     // BaseCase Need to be written here
+    //     if(i > j) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
         
-        int maxi = INT_MIN;
-        for(int k = i ; k <= j ; k++ )
-        {
-            int val = (nums[i-1]*nums[k]*nums[j+1] + cntCoins(nums,k+1,j,dp) + cntCoins(nums,i,k-1,dp));
-            maxi = max(maxi,val);
-        }
-        return dp[i][j] = maxi;
-    }
+    //     int maxi = INT_MIN;
+    //     for(int k = i ; k <= j ; k++ )
+    //     {
+    //         int val = (nums[i-1]*nums[k]*nums[j+1] + cntCoins(nums,k+1,j,dp) + cntCoins(nums,i,k-1,dp));
+    //         maxi = max(maxi,val);
+    //     }
+    //     return dp[i][j] = maxi;
+    // }
 
     // Use The Partitions in this code
 
@@ -27,9 +27,33 @@ public:
             champ.push_back(nums[i]);
         }
         champ.push_back(1);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return cntCoins(champ,1,n,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
+
+        // BaseCase Is Written Here
+        for(int i = 0 ; i <= n ; i++ )
+        {
+            for(int j = 0 ; j <= n ; j++ )
+            {
+                if(i > j) dp[i][j] = 0;
+            }
+        }
+
+        for(int i = n ; i > 0 ; i-- )
+        {
+            for(int j = i ; j <= n ; j++ )
+            {
+                int maxi = INT_MIN;
+                for(int k = j ; k >= i ; k-- )
+                {
+                   int val = (champ[i-1]*champ[k]*champ[j+1] + dp[i][k-1] + dp[k+1][j]);
+                   maxi = max(maxi,val);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+
+        return dp[1][n]+1;
     }
 };
 
-// Now Apply DP to this Code
+// Now Write the Tabulation Solution for this
