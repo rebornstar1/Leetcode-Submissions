1afd6fc63a2169a89b746178ce69ed8500eeabdf
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-07-16 10:11:05 +0530

    Time: 872 ms (15.60%) | Memory: 185.4 MB (9.33%) - LeetSync

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string GetWay(TreeNode* root,int value,string &s)
+    {
+        if(root == NULL) return "test";
+        if(root->val == value) return s;
+
+        s += 'L';
+        string temp1 = GetWay(root->left,value,s);
+        if(temp1 != "test")
+        {
+            return temp1;
+        }
+        s[s.size()-1] = 'R';
+        string temp2 = GetWay(root->right,value,s);
+        if(temp2 != "test")
+        {
+            return temp2;
+        }
+        int num = s.size();
+        s = s.substr(0,num-1);
+        return "test";
+    }
+
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string s = "";
+        string a = GetWay(root,startValue,s);
+        s= "";
+        string b = GetWay(root,destValue,s);
+
+        string ans;
+        int ind = 0;
+        while(a[ind] == b[ind])
+        {
+            ind++;
+        }
+        for(int i = ind ; i < a.size() ; i++ )
+        {
+            ans += 'U';
+        }
+        for(int i = ind ; i < b.size() ; i++ )
+        {
+            ans += b[i];
+        }
+        return ans;
+    }
+};
