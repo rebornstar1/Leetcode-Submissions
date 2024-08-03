263e3d676026a6554e9f0e7eec625416033a0106
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-03 12:16:51 +0530

    Time: 8 ms (66.96%) | Memory: 17.8 MB (33.93%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        unordered_map<int,int>mp;
+        vector<int>mp(1001, 0);
         int n = target.size();
         for(int i = 0 ; i < n ; i++ )
         {
@@ -11,10 +11,8 @@ public:
 
         for(auto it : mp)
         {
-            if(it.second != 0) return false;
+            if(it != 0) return false;
         }
         return true;
     }
 };
-
-// This is O(NlogN) Aprroach
