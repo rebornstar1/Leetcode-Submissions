c71597d281b121132011625f39a465a5123cefe2
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-04 11:43:45 +0530

    Time: 100 ms (22.70%) | Memory: 43.1 MB (6.52%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<long long>prefix,order;
+        prefix.push_back(0);
+        int ind = 0;
+        for(int i = 0 ; i < nums.size() ; i++ )
+        {
+            prefix.push_back(nums[i]+prefix[ind]);
+            ind++;
+        }
+
+        for(int i = 1 ; i <= n ; i++ )
+        {
+            for(int j = i ; j <= n ; j++)
+            {
+                order.push_back(prefix[j]-prefix[i-1]);
+            }
+        }
+
+        sort(order.begin(),order.end());
+        long long ans = 0;
+        for(int i = 0 ; i < order.size() ; i++ )
+        {
+            if(i>=(left-1) && i<=(right-1))
+            {
+                ans = (ans+order[i])%1000000007;
+            }
+        }
+        return ans;
+    }
+};
+
+// First I'm Gonna Write the naive approach
