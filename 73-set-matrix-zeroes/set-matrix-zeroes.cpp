3e2ff3ea14748e8306d4daf0d3ec33c1735c5558
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-05 23:47:16 +0530

    Time: 28 ms (5.29%) | Memory: 16.7 MB (46.86%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        long long ubha = -1, adwa = -1;
+        for(int i = 0 ; i < n ; i++ )
+        {
+            for(int j = 0 ; j < m ; j++ )
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if(j == 0) ubha = 0;
+                    if(i == 0) adwa = 0;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            for(int j = 0 ; j < m ; j++ )
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+        for(int i = 1 ; i < n ; i++ )
+        {
+            if(matrix[i][0] == 0)
+            {
+                for(int j = 1 ; j < m ; j++ )
+                {
+                   matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 1 ; j < m ; j++ )
+        {
+            if(matrix[0][j] == 0)
+            {
+                for(int i = 1 ; i < n ; i++ )
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            if(ubha == 0 || matrix[i][0] == 0)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+        for(int j = 0 ; j < m ; j++ )
+        {
+            if(adwa == 0 || matrix[0][j] == 0)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
