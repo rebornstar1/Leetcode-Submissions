9902721be144bad982f86d19f0681f137f4947e5
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-12 03:42:12 +0530

    Time: 136 ms (5.02%) | Memory: 250.4 MB (5.01%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solve(int amount,int ind,vector<int>&coins,vector<vector<int>>&dp)
+    {
+        if(amount == 0) return 1;
+        if(ind == coins.size() || amount < 0) return 0;
+
+        if(dp[amount][ind] != -1) 
+        {
+            return dp[amount][ind];
+        }
+        int jump = amount;
+
+        int ans = 0;
+        ans += solve(amount,ind+1,coins,dp);
+
+        while(amount > 0)
+        {
+            amount -= coins[ind];
+            ans += solve(amount,ind+1,coins,dp);
+        }
+        return dp[jump][ind] = ans;
+    }
+
+    int change(int amount, vector<int>& coins) {
+        sort(coins.begin(),coins.end());
+        vector<vector<int>>dp(5005,vector<int>(305,-1));
+        return solve(amount,0,coins,dp);
+    }
+};
+
+// Write the Simple Recusive Code
