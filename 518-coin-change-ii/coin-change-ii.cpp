2de2264f88d0dbbe477967acc3f9acfd7a65e670
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-12 12:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.84%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    int solve(int amount,int ind,vector<int>&coins,vector<vector<int>>&dp)
-    {
-        if(amount == 0) return 1;
-        if(ind == coins.size() || amount < 0) return 0;
-
-        if(dp[amount][ind] != -1) 
-        {
-            return dp[amount][ind];
-        }
-        int jump = amount;
-
-        int ans = 0;
-        ans += solve(amount,ind+1,coins,dp);
-
-        while(amount > 0)
-        {
-            amount -= coins[ind];
-            ans += solve(amount,ind+1,coins,dp);
-        }
-        return dp[jump][ind] = ans;
-    }
-
     int change(int amount, vector<int>& coins) {
-        sort(coins.begin(),coins.end());
-        vector<vector<int>>dp(5005,vector<int>(305,-1));
-        return solve(amount,0,coins,dp);
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1; // Base case: There is one way to make amount 0 (using no coins)
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
     }
 };
 
