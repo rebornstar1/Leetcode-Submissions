cb8c84b1506294f4a22a8a0dafbe9a3a8c8f3185
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-14 00:31:10 +0530

    Time: 2276 ms (5.01%) | Memory: 12.4 MB (94.25%) - LeetSync

---
+++ 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int func(vector<vector<int>>& matrix, int i,int j)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        if( i >= 0 && i < n && j >= 0 && j < m)
+        {
+           return matrix[i][j];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int w) {
+        int n = matrix.size(), m = matrix[0].size();
+
+        // n means rows
+        // m means columns
+
+        int ans = INT_MIN;
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            for(int j = 0 ; j < m ; j++ )
+            {
+                matrix[i][j] += (func(matrix,i-1,j)+func(matrix,i,j-1)-func(matrix,i-1,j-1));
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            for(int j = 0 ; j < m ; j++ )
+            {
+                for(int k = i ; k < n ; k++ )
+                {
+                    for(int s = j ; s < m ; s++)
+                    {
+                       int val = (func(matrix,k,s)+func(matrix,i-1,j-1)-func(matrix,k,j-1)-func(matrix,i-1,s));
+                       if(val <= w)
+                       {
+                          ans = max(ans,val);
+                       }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
