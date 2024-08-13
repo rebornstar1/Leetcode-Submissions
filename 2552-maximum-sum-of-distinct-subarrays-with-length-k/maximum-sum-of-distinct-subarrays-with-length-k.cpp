9c17a6d9c8c9c1e9b7f2462f0937b687a8f978dd
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-13 12:58:29 +0530

    Time: 494 ms (5.01%) | Memory: 105.4 MB (9.90%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<long long,long long>mp; // To Keep the Count of Numbers in the array
+        long long sum = INT_MIN, ind = 0, curr = 0;
+        for(int i = 0 ; i < n ; i++ )
+        {
+            if(mp[nums[i]] == 1)
+            {
+                int prev = (i-ind);
+                while(mp[nums[i]] == 1)
+                {
+                    mp[nums[prev]]--;
+                    ind--;
+                    curr -= nums[prev];
+                    prev++;
+                }
+            }
+
+            curr += nums[i];
+            ind++;
+            mp[nums[i]]++;
+
+            cout<<curr<<" "<<ind<<endl;
+            
+            if(ind == k)
+            {
+               sum = max(sum,curr);
+               ind--;
+               curr -= nums[i-k+1];
+               mp[nums[i-k+1]]--;
+            }
+        }
+        return sum != INT_MIN ? sum : 0;
+    }
+};
+
+// Standard Two Pointer Problem
