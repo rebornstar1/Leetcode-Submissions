9802ad1591394e7eaee852b72a5da9ccb34c1e58
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-13 12:11:14 +0530

    Time: 27 ms (57.78%) | Memory: 30.9 MB (8.15%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,55 +1,28 @@
 class Solution {
 public:
-    bool checksum(int target, vector<int>&nums,int m)
-    {
-         int sum = 0;
-         int ans = 0;
-         for(int i = 0 ; i < m ; i++ )
-         {
-            sum += nums[i];
-         }
-         ans = max(ans,sum);
-         for(int i = m ; i < nums.size() ; i++ ){
-            sum+=nums[i];
-            sum-=nums[i-m];
-            ans = max(ans,sum);
-            cout<<sum<<" ";
-         }
-         //cout<<endl;
-
-         return ans >= target;
-    }
-
-    // Let's Check for the first False Condition Now
     int minSubArrayLen(int target, vector<int>& nums) {
+
         int n = nums.size();
-        int s = 1, e = n , m;
-        while((e-s) > 1 )
+        
+        // We Can Apply the Two Pointer Approach in this problem
+        int prev = 0, curr = 0, sum = nums[0], ans = INT_MAX;
+        while(1)
         {
-           m= (s+e)/2;
-           cout<<e<<" "<<s<<" "<<m<<endl;
-           if(checksum(target,nums,m))
-           {
-              e = m;
-           }
-           else
-           {
-             s = (m+1);
-           }
+            if(sum >= target)
+            {
+               ans = min(ans,curr-prev+1);
+               sum-=nums[prev];
+               prev++;
+            }
+            else
+            {
+              if(curr == (n-1)) break;
+              curr++;
+              sum += nums[curr];
+            }
         }
-        //cout<<e<<" "<<s<<" "<<m<<endl;
-        //cout<<checksum(target,nums,m)<<endl;
-        if(checksum(target,nums,s))
-        {
-           return s;
-        }
-        else if(checksum(target,nums,e))
-        {
-           return e;
-        }
-        else
-        {
-            return 0;
-        }
+
+        if(ans == INT_MAX) return 0;
+        return ans;
     }
 };
