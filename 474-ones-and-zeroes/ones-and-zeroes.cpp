c68ef0e208a6c666686aaa66f45c30fa7278fca0
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-13 10:48:47 +0530

    Time: 1611 ms (5.04%) | Memory: 414.4 MB (5.17%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,7 +1,6 @@
 class Solution {
 private:
     int ans = 0;
-
 public:
     int solve(vector<pair<int,int>>&vp, int m, int n, int a, int b, int ind,vector<vector<vector<int>>>&dp)
     {
@@ -20,24 +19,51 @@ public:
 
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<pair<int,int>>vp;
-        vector<vector<vector<int>>>dp(601,vector<vector<int>>(101,vector<int>(101,-1)));
+        vector<vector<vector<int>>>dp(601,vector<vector<int>>(101,vector<int>(101,0)));
+        int N = strs.size();
+
+        // Counting of 1's and 0's
         for(int i = 0 ; i < strs.size() ; i++ )
         {
-            int a = 0, b = 0;
+            int x = 0, y = 0;
             for(int j = 0 ; j < strs[i].size() ; j++ )
             {
-                 if(strs[i][j] == '0')
-                 {
-                   a++; // count of 0's
-                 }
-                 else
-                 {
-                   b++; // count of 1's
-                 }
+                if(strs[i][j] == '0')
+                {
+                    x++;
+                }
+                else
+                {
+                    y++;
+                }
             }
-            vp.push_back({a,b});
+            vp.push_back({x,y});
+        }
+        
+        // Tabulation Solution for this Approach
+        dp[0][0][0] = 0;
+
+        for(int i = 1 ; i <= vp.size() ; i++ )
+        {
+            for(int j = 0 ; j <= m ; j++ )
+            {
+                for(int k = 0 ; k <= n ; k++ )
+                {
+                    dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k]);
+
+                    if((j-vp[i-1].first) >= 0 && (k-vp[i-1].second) >= 0)
+                    {
+                        dp[i][j][k] = max(dp[i][j][k],1+dp[i-1][j-vp[i-1].first][k-vp[i-1].second]);
+                    }
+
+                    ans = max(ans,dp[i][j][k]);
+                }
+            }
         }
 
-        return solve(vp,m,n,0,0,0,dp);
+        return ans;
     }
 };
+
+
+// Write The Tabulation Approach for this
