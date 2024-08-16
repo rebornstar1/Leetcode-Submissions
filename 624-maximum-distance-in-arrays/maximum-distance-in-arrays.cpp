f3f893454fa4340ce2c491cab0eece7790a3162e
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-16 23:59:41 +0530

    Time: 624 ms (5.19%) | Memory: 178.3 MB (5.19%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int m = arrays.size();
+        multiset<int>st;
+        for(int i = 0 ; i < m ; i++ )
+        {
+            int u = arrays[i].size();
+            st.insert(arrays[i][0]);
+            st.insert(arrays[i][u-1]);
+        }
+
+        int ans = INT_MIN;
+        for(int i = 0 ; i < m ; i++ )
+        {
+            int u = arrays[i].size();
+            auto it = st.find(arrays[i][u-1]);
+            st.erase(it);
+            auto kp = st.end();
+            kp--;
+            ans = max(ans,abs(*kp-arrays[i][0]));
+            st.insert(arrays[i][u-1]);
+        }
+
+        for(int i = 0 ; i < m ; i++ )
+        {
+            int u = arrays[i].size();
+            auto it = st.find(arrays[i][0]);
+            st.erase(it);
+            auto kp = st.end();
+            kp--;
+            ans = max(ans,abs(*kp-arrays[i][u-1]));
+            st.insert(arrays[i][0]);
+        }
+        return ans;
+    }
+};
