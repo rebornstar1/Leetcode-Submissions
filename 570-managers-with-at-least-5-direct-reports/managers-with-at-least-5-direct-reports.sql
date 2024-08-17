0ba366ad28a5357c9326de02cdd09ceaa05f6fbe
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-17 23:24:54 +0530

    Time: 347 ms (49.10%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 
-SELECT name FROM Employee AS A 
+SELECT A.name FROM Employee AS A 
 LEFT JOIN 
-(SELECT managerId,COUNT(managerId) FROM Employee WHERE Employee.managerId IS NOT NULL GROUP BY Employee.managerId HAVING COUNT(managerId) >= 5) AS B
-ON A.id = B.managerId WHERE B.managerId IS NOT NULL;
+Employee AS B ON A.id = B.managerId WHERE B.managerId IS NOT NULL GROUP BY B.managerId HAVING COUNT(B.managerId) >= 5;
+
+-- This is a Nested Query Need to write this in a better way
