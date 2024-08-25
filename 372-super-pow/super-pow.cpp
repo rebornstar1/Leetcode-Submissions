137fc46f60526e6a2b5ce13047e7439ff028b5b9
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-08-25 11:31:30 +0530

    Time: 8 ms (45.56%) | Memory: 14.4 MB (7.78%) - LeetSync

--- 372-super-pow/super-pow.cpp
+++ 372-super-pow/super-pow.cpp
@@ -1,5 +1,19 @@
 class Solution {
+private:
+    vector<int>preCompute;
 public:
+    void flagCompute(int a)
+    {
+        preCompute.push_back(1);
+        int ind = 1, val = 1;
+        while(ind != 10)
+        {
+           val = (val*a)%1337;
+           preCompute.push_back(val);
+           ind++;
+        }
+    }
+
     int cntSimp(int a,int n)
     {
         int val = 1;
@@ -16,19 +30,22 @@ public:
 
         if(b[ind]%2 == 0)
         {
-           int temp = (cntSimp(a,b[ind]/2))%1337;
+           int temp = preCompute[b[ind]/2]%1337;
            return (((temp*temp)%1337)*(cntSimp(func(a,b,ind-1),10)%1337))%1337;
         }
         else
         {
-           int temp = (cntSimp(a,b[ind]/2))%1337;
+           int temp = preCompute[b[ind]/2]%1337;
            return (((temp*temp)%1337)*((a*cntSimp(func(a,b,ind-1),10))%1337))%1337;
         }
     }
 
     int superPow(int a, vector<int>& b) {
         int n = b.size();
+        flagCompute(a%1337);
         int val = (func(a%1337,b,n-1))%1337;
         return val;
     }
+
+    // Check If this can be more optimised
 };
