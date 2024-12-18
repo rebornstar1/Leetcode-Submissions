a9daf0bcaa9300a7cce3535284b910fbf6fe9f83
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2024-12-18 10:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (9.86%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        // int N = prices.size();
+        // for(int i = 0 ; i < N ; i++)
+        // {
+        //     int discount = 0;
+        //     for(int j = (i+1) ; j  < N ; j++ )
+        //     {
+        //         if(prices[j] <= prices[i])
+        //         {
+        //             discount = prices[j];
+        //             break;
+        //         }
+        //     }
+        //     prices[i] -= discount;
+        // }
+        // return prices;
+
+        // The More better solution in terms of time complexity would be using stack here
+
         int N = prices.size();
-        for(int i = 0 ; i < N ; i++)
+        stack<pair<int,int>>st;
+        for(int i = 0 ; i < N ; i++ )
         {
-            int discount = 0;
-            for(int j = (i+1) ; j  < N ; j++ )
+            if(st.empty())
             {
-                if(prices[j] <= prices[i])
-                {
-                    discount = prices[j];
-                    break;
-                }
+                st.push({prices[i],i});
             }
-            prices[i] -= discount;
+            else if(st.top().first >= prices[i])
+            {
+                while(!st.empty() && st.top().first >= prices[i]){
+                    prices[st.top().second] -= prices[i];
+                    st.pop();
+                }
+                st.push({prices[i],i});
+            }
+            else
+            {
+                st.push({prices[i],i});
+            }
         }
         return prices;
     }
