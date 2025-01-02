e1e58a4f2ffdda0aa59ba5d587b69066904e7e56
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2025-01-02 10:18:23 +0530

    Time: 8 ms (51.42%) | Memory: 70.4 MB (48.22%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        vector<int>prefix;
-        set<char>test;
+    void makeTest(unordered_set<char>&test)
+    {
         test.insert('a');
         test.insert('e');
         test.insert('i');
         test.insert('o');
         test.insert('u');
-        if(test.find(words[0][0]) != test.end() && test.find(words[0][words[0].size()-1]) != test.end())
-        {
-           prefix.push_back(1);
-        }
-        else
-        {
-            prefix.push_back(0);
-        }
+        return;
+    }
 
-        for(int i = 1 ; i < words.size() ; i++ )
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        vector<int>prefix;
+        unordered_set<char>test;
+        makeTest(test);
+        prefix.push_back(0);
+
+        for(int i = 0 ; i < words.size() ; i++ )
         {
             if(test.find(words[i][0]) != test.end() && test.find(words[i][words[i].size()-1]) != test.end())
             {
-                prefix.push_back(prefix[i-1]+1);
+                prefix.push_back(prefix[i]+1);
             }
             else
             {
-                prefix.push_back(prefix[i-1]+0);
+                prefix.push_back(prefix[i]+0);
             }
         }
 
@@ -35,7 +34,7 @@ public:
             int u = queries[i][0];
             int v = queries[i][1];
             
-            int temp = (prefix[v]-prefix[u]);
+            int temp = (prefix[v+1]-prefix[u+1]);
             if(test.find(words[u][0]) != test.end() && test.find(words[u][words[u].size()-1]) != test.end())
             {
                 temp++;
