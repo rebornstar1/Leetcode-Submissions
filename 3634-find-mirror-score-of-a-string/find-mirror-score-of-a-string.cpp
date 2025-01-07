558e4ad9cf8f9daa62d1e198dbae622ad129267b
PAUL SANJAY MANIKRAO <153437781+rebornstar1@users.noreply.github.com>
2025-01-07 15:04:09 +0530

    Time: 11 ms (92.65%) | Memory: 17.5 MB (61.77%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
@@ -3,19 +3,19 @@ public:
     long long calculateScore(string s) {
         long long score = 0;
         int n = s.size();
-        vector<priority_queue<long long>>inc(30);
+        vector<vector<long long>>inc(30);
 
         for(int i = 0 ; i < n ; i++ )
         {
             if(!inc['z'-s[i]].empty())
             {
-                long long index = inc['z'-s[i]].top();
-                inc['z'-s[i]].pop();
+                long long index = inc['z'-s[i]].back();
+                inc['z'-s[i]].pop_back();
                 score += abs(index-i);
             } 
             else
             {
-                inc[s[i]-'a'].push(i);
+                inc[s[i]-'a'].push_back(i);
             }
         }
         return score;
