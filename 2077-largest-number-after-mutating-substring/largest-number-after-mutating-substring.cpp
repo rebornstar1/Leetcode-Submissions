138fbb70f1794c63dfab5b9c763f196d82a75069
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-01-11 17:58:56 +0530

    Time: 4 ms (56.13%) | Memory: 32.4 MB (75.48%) - LeetSync

---
+++ 2077-largest-number-after-mutating-substring/largest-number-after-mutating-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string maximumNumber(string num, vector<int>& change) {
+        bool first_String = false;
+        int n = num.size();
+
+        for(int i = 0 ; i < n ; i++ )
+        {
+            if(first_String)
+            {
+                if((num[i]-'0') > change[num[i]-'0'])
+                {
+                    break;
+                }
+                else
+                {
+                    num[i] = (change[num[i]-'0']+'0');
+                }
+            }
+            else
+            {
+                if((num[i]-'0') < change[num[i]-'0'])
+                {
+                    first_String = true;
+                    num[i] = (change[num[i]-'0']+'0');
+                }
+            }
+        }
+        return num;
+    }
+};
