2847c8283b0897757387c651a91d0199e75f5667
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-01-13 13:05:50 +0530

    Time: 66 ms (82.90%) | Memory: 55.1 MB (8.27%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -1,38 +1,61 @@
+// class Solution {
+// public:
+//     long long solve(vector<int>&strength, int x, int terms, int k,int bits)
+//     {
+//         if(terms == strength.size()) return 0;
+
+//         long long val = INT_MAX;
+//         for(int i = 0 ; i < strength.size() ; i++ )
+//         {
+//             if(!((bits >> i) | 0))
+//             {
+//                 if(strength[i]%x == 0)
+//                 {
+//                   val = min(val,((long long)strength[i]/x) + solve(strength, x+k, terms+1 , k, bits |= (1 << i)));
+//                 }
+//                 else
+//                 {
+//                   val = min(val,((long long)strength[i]/x + 1) + solve(strength, x+k, terms+1, k, bits |= (1 << i)));
+//                 }
+//             }
+//         }
+//         return val;
+//     }
+
+//     int findMinimumTime(vector<int>& strength, int k) {
+//         int n = strength.size();
+//         int x = 1, time = 0;
+//         return solve(strength,x,0,k,0);
+//     }
+// };
+
 class Solution {
 public:
-    int solve(vector<int>&strength, int x, int terms, int k)
-    {
-        if(terms == strength.size()) return 0;
+    unordered_map<int, unordered_map<int, int>> memo; // Memoization table
 
+    int solve(vector<int>& strength, int x, int terms, int k, int bits) {
+        if (terms == strength.size()) return 0; // Base case: all terms used
+        if (memo[bits].count(x)) return memo[bits][x]; // Check memoization
+        
         int val = INT_MAX;
-        for(int i = 0 ; i < strength.size() ; i++ )
-        {
-            if(strength[i] != INT_MAX)
-            {
-                int temp = strength[i];
-                strength[i] = INT_MAX;
+        for (int i = 0; i < strength.size(); i++) {
+            if ((bits >> i) & 1) continue; // Skip if the i-th bit is set (already used)
 
-                if(temp%x == 0)
-                {
-                  val = min(val,(temp/x) + solve(strength, x+k, terms+1 , k)) ;
-                }
-                else
-                {
-                  val = min(val,(temp/x + 1) + solve(strength, x + k, terms+1, k));
-                }
-                strength[i] = temp;
-            }
+            // Set the i-th bit
+            int newBits = bits | (1 << i);
+            
+            // Time calculation
+            int time = (strength[i] + x - 1) / x;
+            val = min(val, time + solve(strength, x + k, terms + 1, k, newBits));
         }
-        return val;
+
+        return memo[bits][x] = val; // Store result in memoization table
     }
 
     int findMinimumTime(vector<int>& strength, int k) {
-        int n = strength.size();
-        int x = 1, time = 0;
-        return solve(strength,x,0,k);
+        return solve(strength, 1, 0, k, 0);
     }
 };
 
-// Your Goal is to remove the Redundant States
 
-// Try Making All permutations of the strength Array here
+// Apply the Bitmasking here
