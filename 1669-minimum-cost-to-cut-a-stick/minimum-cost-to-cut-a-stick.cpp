0377fd19f07abe7e176a63ec8fbeeb401d11296c
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-14 14:59:40 +0530

    Time: 39 ms (32.35%) | Memory: 14 MB (23.69%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int f(vector<int>&cuts,int i,int j,int cs,int ce,vector<vector<int>> &dp)
-    {   
-        if(cs > ce)
-        {
-            return 0;
+    int func(int left, int right, vector<int>& cuts, vector<vector<int>>& dp) {
+        if (right - left <= 1) return 0;
+        
+        if (dp[left][right] != -1) return dp[left][right];
+        
+        int minCost = INT_MAX;
+        
+        for (int k = left + 1; k < right; k++) {
+            int cost = cuts[right] - cuts[left] + 
+                      func(left, k, cuts, dp) + 
+                      func(k, right, cuts, dp);
+            
+            minCost = min(minCost, cost);
         }
-        if(dp[cs][ce] != -1)
-        {
-            return dp[cs][ce];
-        }
-        int mini = INT_MAX;
-        for(int k = cs ; k <= ce ; k++ )
-        {
-            int ans = (f(cuts,i,cuts[k],cs,k-1,dp)+f(cuts,cuts[k],j,k+1,ce,dp)+(j-i));
-            mini = min(ans,mini);
-        }
-        return dp[cs][ce] = mini;
+        
+        return dp[left][right] = minCost;
     }
-
+    
     int minCost(int n, vector<int>& cuts) {
-        int i = 0;
-        int j = n;
-        int ts = cuts.size();
-        sort(cuts.begin(),cuts.end());
-        vector<vector<int>>dp(ts+1,vector<int>(ts+1,-1));
-        return f(cuts,i,j,0,cuts.size()-1,dp);
+        vector<int> allCuts = {0};
+        for (int cut : cuts) allCuts.push_back(cut);
+        allCuts.push_back(n);
+        
+        sort(allCuts.begin(), allCuts.end());
+        
+        int size = allCuts.size();
+        vector<vector<int>> dp(size, vector<int>(size, -1));
+        
+        return func(0, size - 1, allCuts, dp);
     }
 };
-
-// Instead of i and j use starting and ending cuts here
