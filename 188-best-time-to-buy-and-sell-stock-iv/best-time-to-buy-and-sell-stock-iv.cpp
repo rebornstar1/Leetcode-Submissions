53de1b46e57bc8eb3ee64323e860345f2f7ce7c2
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-21 10:11:46 +0530

    Time: 7 ms (61.08%) | Memory: 19.7 MB (15.84%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int evaluate(vector<int>& prices,int k,int isBuy,int index,vector<vector<vector<int>>>&dp)
-    {
-        if(k == 0 || index == prices.size())
-        {
-            return 0;
-        }
-
-        if(dp[k][isBuy][index] != -1) return dp[k][isBuy][index];
-
-        int a,b;
-
-        if(isBuy)
-        {
-           a = (evaluate(prices,k,0,index+1,dp) - prices[index]);
-           b = evaluate(prices,k,1,index+1,dp);
-        }
-        else
-        {
-           a = (evaluate(prices,k-1,1,index+1,dp) + prices[index]);
-           b = evaluate(prices,k,0,index+1,dp);
-        }
-        return dp[k][isBuy][index] = max(a,b);
-    }
-
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
+        vector<vector<vector<int>>>dp(k+5,vector<vector<int>>(3,vector<int>(n+5,0)));
+        
+        for(int i = 1 ; i <= k ; i++ )
+        {
+            for(int j = 0 ; j <= 1 ; j++ )
+            {
+                for(int p = (n-1) ; p >= 0 ; p-- )
+                {
+                    if(j == 0)
+                    {
+                       // Sellable
+                       dp[i][0][p] = max(dp[i-1][1][p+1]+prices[p],dp[i][0][p+1]);
+                    }
+                    else
+                    {
+                       // Buyable
+                       dp[i][1][p] = max(dp[i][0][p+1]-prices[p],dp[i][1][p+1]);
+                    }
+                }
+            }
+        }
 
-        vector<vector<vector<int>>>dp(k+5,vector<vector<int>>(3,vector<int>(n+5,-1)));
-        return evaluate(prices,k,1,0,dp);
+        return dp[k][1][0];
     }
 
-    // 1 means it is buyable 
-    // 0 means it is sellable
+    // Can you think of any optimal solution for this problem ?
 };
