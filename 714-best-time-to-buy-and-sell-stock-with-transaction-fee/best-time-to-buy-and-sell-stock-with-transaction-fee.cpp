1eb969f27fedc9f86611e487fd9535c42312a7a5
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-21 16:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (96.96%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,28 +2,14 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<int>prev(3,0);
-        vector<int>curr(3,0);
+        int hold = -prices[0]; // You bought on day 0
+        int cash = 0;          // You did nothing on day 0
 
-        for(int i = (n-1) ; i >= 0 ; i-- )
-        {
-            for(int j = 0 ; j <= 1 ; j++ )
-            {
-                if(j == 0)
-                {
-                    curr[0] = max(prev[0],prev[1]-prices[i]);
-                }
-                else
-                {
-                    curr[1] = max(prev[1],prev[0]+prices[i]-fee);  
-                }
-            }
-            prev = curr;
+        for (int i = 1; i < n; ++i) {
+            hold = max(hold, cash - prices[i]);
+            cash = max(cash, hold + prices[i] - fee);
         }
-        return prev[0];
+
+        return cash;
     }
 };
-
-// See If you can reduce the space complexity of this problem
-
-// First Converting this solution to iterative approach
