a9cd8707c79c82b7f4c45c7cddea3148e72fba1e
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-21 16:17:49 +0530

    Time: 16 ms (62.45%) | Memory: 59 MB (67.32%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
-    int evaluate(int index,int isBuy,int fee,vector<int>& prices,vector<vector<int>>&dp)
-    {
-        if(index == prices.size()) return 0;
-
-        if(dp[index][isBuy] != -1) return dp[index][isBuy];
-
-        int a,b;
-        if(isBuy)
-        {
-            // Sellable
-            a = evaluate(index+1,1,fee,prices,dp);
-            b = (evaluate(index+1,0,fee,prices,dp)+prices[index]-fee);
-        }
-        else
-        {
-            // Buyable
-            a = evaluate(index+1,0,fee,prices,dp);
-            b = evaluate(index+1,1,fee,prices,dp)-prices[index];
-        }
-        return dp[index][isBuy] = max(a,b);
-    }
-
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>>dp(n+5,vector<int>(3,0));
-
+        vector<int>prev(3,0);
+        vector<int>curr(3,0);
 
         for(int i = (n-1) ; i >= 0 ; i-- )
         {
@@ -33,17 +11,19 @@ public:
             {
                 if(j == 0)
                 {
-                    dp[i][0] = max(dp[i+1][0],dp[i+1][1]-prices[i]);
+                    curr[0] = max(prev[0],prev[1]-prices[i]);
                 }
                 else
                 {
-                    dp[i][1] = max(dp[i+1][1],dp[i+1][0]+prices[i]-fee);  
+                    curr[1] = max(prev[1],prev[0]+prices[i]-fee);  
                 }
             }
+            prev = curr;
         }
-
-        return dp[0][0];
+        return prev[0];
     }
 };
+
+// See If you can reduce the space complexity of this problem
 
 // First Converting this solution to iterative approach
