31e35783d7840fb429b214bb3c58d7231a089dff
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-21 10:15:24 +0530

    Time: 3 ms (82.44%) | Memory: 14.7 MB (75.73%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(k+5,vector<vector<int>>(3,vector<int>(n+5,0)));
-        
+        vector<vector<int>>curr(3,vector<int>(n+5,0));
+        vector<vector<int>>prev(3,vector<int>(n+5,0));
+
         for(int i = 1 ; i <= k ; i++ )
         {
             for(int j = 0 ; j <= 1 ; j++ )
@@ -13,18 +14,21 @@ public:
                     if(j == 0)
                     {
                        // Sellable
-                       dp[i][0][p] = max(dp[i-1][1][p+1]+prices[p],dp[i][0][p+1]);
+                       curr[0][p] = max(prev[1][p+1]+prices[p],curr[0][p+1]);
                     }
                     else
                     {
                        // Buyable
-                       dp[i][1][p] = max(dp[i][0][p+1]-prices[p],dp[i][1][p+1]);
+                       curr[1][p] = max(curr[0][p+1]-prices[p],curr[1][p+1]);
                     }
                 }
             }
+            prev = curr;
         }
 
-        return dp[k][1][0];
+        // I guess we can implement like dp[i] => curr and dp[i-1] => prev
+
+        return curr[1][0];
     }
 
     // Can you think of any optimal solution for this problem ?
