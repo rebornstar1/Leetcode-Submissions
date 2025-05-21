814f4e57de533ee9dcbf8e9050bb07ad17aa7f5f
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-05-21 12:54:12 +0530

    Time: 4 ms (21.08%) | Memory: 16.7 MB (9.31%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evaluate(int index,int isBuy,vector<int>&prices,vector<vector<int>>&dp)
+    {
+        if(index == prices.size()) return 0;
+
+        if(dp[index][isBuy] != -1) return dp[index][isBuy];
+
+        int a,b;
+        if(isBuy == 1)
+        {
+            // Buyable
+           a = (evaluate(index+1,0,prices,dp)-prices[index]);
+           b = evaluate(index+1,1,prices,dp);
+        }
+        else if(isBuy == 0)
+        {
+            // Sellable
+            a = (evaluate(index+1,2,prices,dp)+prices[index]);
+            b = evaluate(index+1,0,prices,dp);
+        }
+        else
+        {
+            return dp[index][isBuy] = evaluate(index+1,1,prices,dp);
+        }
+
+        return dp[index][isBuy] = max(a,b);
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(5,-1));
+        return evaluate(0,1,prices,dp); // (index,isBuy,prices)
+        // isBuy 0 (Sellable)
+        // isBuy 1 (Buyable)
+        // isBuy 2 (Cooldown)
+    }
+};
+
+// I guess we can directly go with the approach of the take and not take like
