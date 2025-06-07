f141c356186d2fe3f339955e6c37fef029e404ce
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-07 20:25:30 +0530

    Time: 194 ms (27.50%) | Memory: 138.3 MB (40.00%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-        
-        // First try to find the Minimum length array till ith index
-        vector<int>lastk(n+1,n+5);
-        vector<int>firstk(n+1,n+5);
 
         unordered_map<int,int>mp;
         mp[0] = 1;
@@ -16,37 +12,19 @@ public:
            mp[arr[i]] = (i+2);
         } 
         
+        int temp = (n+5), ans = INT_MAX; // impossible value
         for(int i = 0 ; i < n ; i++ )
         {
+            if(i > 0 && mp.find(arr[i-1]+target) != mp.end() && mp[arr[i-1]+target] != 0)
+            {
+                ans = min(ans,temp+mp[arr[i-1]+target]-1-i);
+            }
+
+            // and now update the ending one here
             if(mp.find(arr[i]-target) != mp.end() && mp[arr[i]-target] != 0)
             {
-                lastk[i] = (mp[arr[i]]-mp[arr[i]-target]);
+                temp = min(temp,mp[arr[i]]-mp[arr[i]-target]);
             }
-        }
-
-        for(int i = 0 ; i < n ; i++ )
-        {
-            if(mp.find(arr[i]+target) != mp.end() && mp[arr[i]+target] != 0)
-            {
-                firstk[i+1] = (-mp[arr[i]]+mp[arr[i]+target]);
-            }
-        }
-
-        for(int i = (n-2) ; i >= 0 ; i-- )
-        {
-            firstk[i] = min(firstk[i],firstk[i+1]);
-        }
-
-        for(int i = 1 ; i < n ; i++ )
-        {
-            lastk[i] = min(lastk[i],lastk[i-1]);
-        }
-
-        int ans = INT_MAX;
-
-        for(int i = 0 ; i < (n-1) ; i++ )
-        {
-             ans = min(ans,lastk[i]+firstk[i+1]);
         }
 
         if(ans > n) return -1;
