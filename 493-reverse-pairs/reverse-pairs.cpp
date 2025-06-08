c6a07ab11bbc2fb826df4e0a267afb872da26821
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-08 13:09:51 +0530

    Time: 1348 ms (5.02%) | Memory: 223.5 MB (69.93%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,66 @@
+class Solution {
+private:
+    int ans = 0;
+public:
+    void merge(int l,int m,int r,vector<int>&nums)
+    {
+        vector<int>left;
+        vector<int>right;
+        for(int i = l ; i <= m ; i++ )
+        {
+            left.push_back(nums[i]);
+        }
+        for(int i = (m+1) ; i <= r ; i++ )
+        {
+            right.push_back(nums[i]);
+        }
+
+        int index = l, alpha = 0, beta = 0;
+        
+        int flip = 0;
+        for (int i = 0; i < left.size(); i++) {
+            while (flip < right.size() && (long long)left[i] > 2LL * right[flip]) {
+                flip++;
+            }
+            ans += flip;
+        }
+
+
+        while(alpha < left.size() && beta < right.size()) {
+
+            // find the count here itself
+            if(left[alpha] > right[beta]) {
+                nums[index++] = right[beta++];
+            } else {
+                nums[index++] = left[alpha++];
+            }
+        }
+
+        while(alpha < left.size()) {
+            nums[index++] = left[alpha++];
+        }
+
+        while(beta < right.size()) {
+            nums[index++] = right[beta++];
+        }
+
+    }
+
+    void mergeSort(int l,int r,vector<int>&nums)
+    {
+        if((r-l)<1) return;
+        int m = (l+r)/2;
+        mergeSort(l,m,nums);
+        mergeSort(m+1,r,nums);
+        merge(l,m,r,nums);
+    }
+
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        mergeSort(0,n-1,nums);
+        for(int i = 0 ; i < n ; i++ ) cout<<nums[i]<<" ";
+        return ans;
+    }
+};
+
+// first write the whole code for the merge sort itself
