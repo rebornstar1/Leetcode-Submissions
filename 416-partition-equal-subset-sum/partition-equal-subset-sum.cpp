a3e1305b2f18fd08124da119c555ce48e2ed47b3
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-10 22:23:00 +0530

    Time: 230 ms (8.95%) | Memory: 205.6 MB (7.56%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool func(int index,int target, vector<int>& nums, vector<vector<int>>&dp)
+    {
+        if(target < 0) return false;
+        if(index == nums.size()) 
+        {
+            if(target == 0) return true;
+            return false;
+        }
+
+        if(dp[index][target] != -1) return dp[index][target];
+
+        return dp[index][target] = func(index+1,target-nums[index],nums,dp) | func(index+1,target,nums,dp);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(), sum = 0;
+        for(int i = 0 ; i < n ; i++ ) sum += nums[i];
+        if(sum%2 == 1) return false;
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        return func(0,sum/2,nums,dp);
+    }
+};
