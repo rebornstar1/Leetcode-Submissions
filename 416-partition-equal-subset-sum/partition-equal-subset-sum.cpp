fc769e0a1126e50de71d080dee1333d8be595326
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-10 22:43:27 +0530

    Time: 181 ms (17.57%) | Memory: 205.5 MB (7.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    bool func(int index,int target, vector<int>& nums, vector<vector<int>>&dp)
-    {
-        if(target < 0) return false;
-        if(target == 0) return true;
-        if(index == nums.size()) return false;
-
-        if(dp[index][target] != -1) return dp[index][target];
-
-        return dp[index][target] = func(index+1,target-nums[index],nums,dp) | func(index+1,target,nums,dp);
-    }
-
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), sum = 0;
         for(int i = 0 ; i < n ; i++ ) sum += nums[i];
         if(sum%2 == 1) return false;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return func(0,sum/2,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+
+        for(int i = 0 ; i <= n ; i++ ) dp[i][0] = 1;
+
+        for(int i = (n-1) ; i >= 0 ; i-- )
+        {
+            for(int j = 0 ; j <= (sum/2) ; j++ )
+            {
+                int val1 = ((j-nums[i]) >= 0 ? dp[i+1][j-nums[i]] : 0);
+                int val2 = dp[i+1][j];
+                dp[i][j] = (val1 | val2); 
+            }
+        }
+        return dp[0][sum/2];
     }
 };
+
+// Space Optimization could be possible
