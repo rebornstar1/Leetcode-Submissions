e67f79bcfef7b3337ae241f083572abddeeab2c2
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-12 17:47:49 +0530

    Time: 40 ms (18.46%) | Memory: 13.9 MB (77.07%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int func(int left, int right, vector<int>& cuts, vector<vector<int>>& dp) {
-        if (right - left <= 1) return 0;
-        
-        if (dp[left][right] != -1) return dp[left][right];
-        
-        int minCost = INT_MAX;
-        
-        for (int k = left + 1; k < right; k++) {
-            int cost = cuts[right] - cuts[left] + 
-                      func(left, k, cuts, dp) + 
-                      func(k, right, cuts, dp);
-            
-            minCost = min(minCost, cost);
+    vector<vector<int>> dp;
+
+    int func(int si, int ei, vector<int>& cuts) {
+        if (si > ei) return 0;
+        if (dp[si][ei] != -1) return dp[si][ei];
+
+        int ans = INT_MAX;
+        for (int i = si; i <= ei; ++i) {
+            int cost = cuts[ei + 1] - cuts[si - 1];
+            ans = min(ans, func(si, i - 1, cuts) + func(i + 1, ei, cuts) + cost);
         }
-        
-        return dp[left][right] = minCost;
+
+        return dp[si][ei] = ans;
     }
-    
+
     int minCost(int n, vector<int>& cuts) {
-        vector<int> allCuts = {0};
-        for (int cut : cuts) allCuts.push_back(cut);
-        allCuts.push_back(n);
-        
-        sort(allCuts.begin(), allCuts.end());
-        
-        int size = allCuts.size();
-        vector<vector<int>> dp(size, vector<int>(size, -1));
-        
-        return func(0, size - 1, allCuts, dp);
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+
+        int m = cuts.size();
+        dp = vector<vector<int>>(m, vector<int>(m, -1));
+
+        return func(1, m - 2, cuts);
     }
 };
