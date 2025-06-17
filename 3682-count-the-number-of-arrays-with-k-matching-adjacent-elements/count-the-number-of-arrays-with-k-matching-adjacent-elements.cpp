7ba7e66ab03ee0df45a6f4e8e3bc559be2134b64
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-06-17 15:22:54 +0530

    Time: 131 ms (43.18%) | Memory: 8.6 MB (87.50%) - LeetSync

---
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    int M = 1000000007;
+public:
+    int func(int num,int power)
+    {
+        if(power == 0) return 1;
+
+        if(power%2 == 0)
+        {
+            long long temp = func(num,power/2)%M;
+            return (temp*temp)%M;
+        }
+        else
+        {
+            long long temp = func(num,power/2)%M;
+            long long val = (temp*num)%M;
+            return (temp*val)%M; 
+        }
+    }
+
+    int countGoodArrays(int n, int m, int k) {
+        long long val1 = 1, temp1 = (n-k-1);
+        while(temp1--) val1 = (val1*(m-1))%M;
+
+        long long val2 = 1;
+        for(int i = 1 ; i <= (n-1) ; i++ ) val2 = (val2*i)%M;
+
+        long long val3 = 1;
+        for(int i = 1 ; i <= (n-1-k) ; i++ ) val3 = (val3*i)%M;
+
+        long long val4 = 1;
+        for(int i = 1 ; i <= k ; i++ ) val4 = (val4*i)%M;
+
+        long long val5 = (val3*val4)%M;
+        val5 = func(val5,M-2);
+
+        return ((((m%M)*(val1%M))%M)*(((val2%M)*(val5%M))%M))%M;
+    }
+};
