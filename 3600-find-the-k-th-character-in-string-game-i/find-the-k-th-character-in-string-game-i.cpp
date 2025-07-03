a8533d27cceab0acab6077f125ac10670abe87de
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-07-03 11:12:51 +0530

    Time: 7 ms (16.92%) | Memory: 11.4 MB (13.20%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        // findPow Of Two's
+        set<int>powOfTwo;
+        int temp = 1;
+        for(int i = 1 ; i < 30 ; i++ )
+        {
+            powOfTwo.insert(temp);
+            temp *= 2;
+        }
+
+        vector<int>dp(k+1,0);
+        dp[0] = 0;
+        for(int i = 0 ; i <= k ; i++ )
+        {
+           // use the upper bound in this
+           auto it = powOfTwo.upper_bound(i);
+           int tempVal = (*it);
+
+           while(1)
+           {
+               if((i+tempVal) <= k)
+               {
+                  dp[i+tempVal] = (dp[i]+1);
+                  tempVal *= 2;
+               }
+               else
+               {
+                  break;
+               }
+           }
+        }
+        return (dp[k-1]%26+'a');
+    }
+};
