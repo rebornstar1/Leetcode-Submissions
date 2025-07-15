a1778a9b75a2d0cdb030c4d9692ff71a06c1cdd9
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-07-15 12:50:07 +0530

    Time: 47 ms (5.87%) | Memory: 22.4 MB (5.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // You Need To Create An Adjacnecy List Here
-        vector<vector<pair<int,int>>>adj(n+1);
-
+        vector<vector<vector<int>>>Adj(n+1);
         for(int i = 0 ; i < flights.size() ; i++ )
         {
-            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
-        }
-        
-        // Adjacency List Created Successfully
-        vector<int>dist(n+1,INT_MAX);
+            int from = flights[i][0];
+            int to = flights[i][1];
+            int price = flights[i][2];
 
-       // In This Question Number of Stops Matters to me
-       queue<vector<int>>qst;
-       qst.push({0,src}); // 1 => distance 2 => Index of Node
-       dist[src] = 0;
-       int a = 0;
-       while(!qst.empty() && a <= k)
-       {
-            int p = qst.size();
-            while(p--){
-                int val = qst.front()[0];
-                int inx = qst.front()[1];
-                qst.pop();
-                cout<<val<<" "<<inx<<endl;
-                
-                for(int j = 0 ; j < adj[inx].size() ; j++ )
-                {
-                    if((dist[adj[inx][j].first]) > (adj[inx][j].second + val))
-                    {
-                       dist[adj[inx][j].first] =  (adj[inx][j].second + val);
-                       qst.push({dist[adj[inx][j].first],adj[inx][j].first});
-                    }
+            Adj[from].push_back({to,price});
+        }
+
+        vector<vector<int>>dist(n+1,vector<int>(k+5,INT_MAX));
+
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        pq.push({0,src,k+1});
+        dist[src][k+1] = 0;
+
+        int ans = INT_MAX;
+
+        while(!pq.empty())
+        {
+            int curr_val = pq.top()[0];
+            int curr_node = pq.top()[1];
+            int rem_flight = pq.top()[2];
+            pq.pop();
+
+            if(rem_flight == 0) continue; // we cannot fly further
+
+            for(int i = 0 ; i < Adj[curr_node].size() ; i++ )
+            {
+                int to = Adj[curr_node][i][0];
+                int price = Adj[curr_node][i][1];
+
+                if (curr_val + price < dist[to][rem_flight - 1]) {
+                    dist[to][rem_flight - 1] = curr_val + price;
+                    pq.push({dist[to][rem_flight - 1], to, rem_flight - 1});
                 }
             }
-            a++;
-       }
+        }
 
-       if(dist[dst] == INT_MAX) return -1;
-       return dist[dst];
+        for(int i = 0 ; i <= k ; i++ ) ans = min(ans,dist[dst][i]);
+
+        return ans == INT_MAX ? -1 : ans;
     }
 };
+
+// A 2-D DP way of approach needs to be used here itself
