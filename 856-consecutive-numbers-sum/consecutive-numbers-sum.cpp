3171f5d71e8855bc82919cf28b06498d898b15b4
Sanjay Manikrao Paul <153437781+rebornstar1@users.noreply.github.com>
2025-07-21 12:59:03 +0530

    Time: 7 ms (6.55%) | Memory: 18.4 MB (5.09%) - LeetSync

--- 856-consecutive-numbers-sum/consecutive-numbers-sum.cpp
+++ 856-consecutive-numbers-sum/consecutive-numbers-sum.cpp
@@ -3,9 +3,11 @@ public:
     int consecutiveNumbersSum(int n) {
         // first pre compute for the numbers till 1e5 itself
 
-        vector<long long>preCompute(1e5+1,0);
+        int sizze = sqrt(2*n)+1;
+
+        vector<long long>preCompute(sizze,0);
         int ans = 0;
-        for(int i = 1 ; i < 1e5 ; i++ )
+        for(int i = 1 ; i < sizze ; i++ )
         {
             preCompute[i] = (i+preCompute[i-1]);
             if((n-preCompute[i])%i == 0 && n >= preCompute[i]) ans++; 
@@ -13,3 +15,5 @@ public:
         return ans;
     }
 };
+
+// see if we can optimize this solution 
